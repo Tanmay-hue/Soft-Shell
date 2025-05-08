a79df9e1e16ed24c551ea9c8a3222489f59f892e
Tanmay Singh <133014692+Tanmay-hue@users.noreply.github.com>
2025-05-08 23:06:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,7 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: './',
 };
 
 export default nextConfig;
